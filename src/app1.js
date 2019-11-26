cdeef0dd218b627a6c690b9f3ed93558b3799b42
frankfang <frankfang1990@gmail.com>
2019-11-26 11:47:41 +0800

    引入 Vue

--- src/app1.js
+++ src/app1.js
@@ -1,63 +1,51 @@
 import './app1.css'
-import $ from 'jquery'
-import Model from './base/Model.js'
-import View from './base/View'
-import EventBus from './base/EventBus'
+import Vue from 'vue'
 
-
-// 数据相关都放到m
-const m = new Model({
-  data: {
-    n: parseFloat(localStorage.getItem('n'))
-  },
-  update: function (data) {
-    Object.assign(m.data, data)
-    m.trigger('m:updated')
-    localStorage.setItem('n', m.data.n)
-  }
-})
-// 其他都c
 const init = (el) => {
-  new View({
+
+  const m = {
+    get() {
+      return parseFloat(localStorage.getItem('n'))
+    },
+    set(n) {
+      localStorage.setItem('n', n)
+    }
+  }
+  new Vue({
     el: el,
-    data: m.data,
-    html: `
-      <div>
+    data: {n: m.get()},
+    methods: {
+      add() {
+        this.n += 1
+      },
+      minus() {
+        this.n -= 1
+      },
+      mul() {
+        this.n *= 2
+      },
+      div() {
+        this.n /= 2
+      },
+    },
+    watch: {
+      n: function () {
+        m.set(this.n)
+      },
+    },
+    template: `
+      <section>
         <div class="output">
           <span id="number">{{n}}</span>
         </div>
         <div class="actions">
-          <button id="add1">+1</button>
-          <button id="minus1">-1</button>
-          <button id="mul2">*2</button>
-          <button id="divide2">÷2</button>
+          <button @click="add">+1</button>
+          <button @click="minus">-1</button>
+          <button @click="mul">*2</button>
+          <button @click="div">÷2</button>
         </div>
-      </div>
-    `,
-    render(data) {
-      const n = data.n
-      if (this.el.children.length !== 0) this.el.empty()
-      $(this.html.replace('{{n}}', n))
-        .appendTo(this.el)
-    },
-    events: {
-      'click #add1': 'add',
-      'click #minus1': 'minus',
-      'click #mul2': 'mul',
-      'click #divide2': 'div',
-    },
-    add() {
-      m.update({n: m.data.n + 1})
-    },
-    minus() {
-      m.update({n: m.data.n - 1})
-    },
-    mul() {
-      m.update({n: m.data.n * 2})
-    },
-    div() {
-      m.update({n: m.data.n / 2})
-    }
+      </section>
+    `
   })
 }
 
--- src/app2.js
+++ src/app2.js
@@ -1,55 +1,26 @@
 import './app2.css'
-import $ from 'jquery'
-import Model from './base/Model'
-import View from './base/View'
-import EventBus from './base/EventBus'
-
-const eventBus = new EventBus()
-
-const localKey = 'app2.index'
-const m = new Model({
-  data: {
-    index: parseInt(localStorage.getItem(localKey)) || 0
-  },
-  update(data) {
-    Object.assign(m.data, data)
-    eventBus.trigger('m:updated')
-    localStorage.setItem('app2.index', m.data.index)
-  },
-})
-
+import Vue from 'vue'
 
 const init = (el) => {
-  new View({
+  new Vue({
     el: el,
-    data: m.data,
-    eventBus: eventBus,
-    html: (index) => {
-      return `
-    <div>
-      <ol class="tab-bar">
-        <li class="${index === 0 ? 'selected' : ''}" data-index="0"><span>1111</span></li>
-        <li class="${index === 1 ? 'selected' : ''}" data-index="1"><span>2222</span></li>
-      </ol>
-      <ol class="tab-content">
-        <li class="${index === 0 ? 'active' : ''}">内容1</li>
-        <li class="${index === 1 ? 'active' : ''}">内容2</li>
-      </ol>
-    </div>
-`
+    data: {
+      index: 0
     },
-    render(data) {
-      const index = data.index
-      if (this.el.children.length !== 0) this.el.empty()
-      $(this.html(index)).appendTo(this.el)
-    },
-    events: {
-      'click .tab-bar li': 'x',
-    },
-    x(e) {
-      const index = parseInt(e.currentTarget.dataset.index)
-      m.update({index: index})
-    },
+    template: `
+      <section id="app2">
+        <ol class="tab-bar">
+          <li :class="index === 0 ? 'selected' : ''"
+            @click="index=0"><span>1111</span></li>
+          <li :class="index === 1 ? 'selected' : ''"
+            @click="index=1"><span>2222</span></li>
+        </ol>
+        <ol class="tab-content">
+          <li :class="index === 0 ? 'active': ''" >内容1</li>
+          <li :class="index === 1? 'active': ''" >内容2</li>
+        </ol>
+      </section>
+    `
   })
 }
 
